9a5377a399ebf10e0a88627af993695407bfb4a1
Nico Bustelo <nbustelo2002@gmail.com>
2023-12-18 05:04:43 -0300

    exercises 1.6-1.11 finished

--- part1/part1/src/App.jsx
+++ part1/part1/src/App.jsx
@@ -1,66 +1,78 @@
-const Header = props => {
+import { useState } from 'react';
+
+const Title = ({ text }) => {
 	return (
 		<>
-			<h1>{props.course}</h1>
+			<h1>{text}</h1>
 		</>
 	);
 };
 
-const Content = props => {
+const Button = ({ eventHandler, text }) => {
+	return <button onClick={eventHandler}>{text}</button>;
+};
+
+const StatisticLine = ({ text, value }) => {
 	return (
 		<>
-			<p>
-				{props.part} {props.excersices}
-			</p>
+			<tr>
+				<td>{text}</td>
+				<td>{value}</td>
+			</tr>
 		</>
 	);
 };
 
-const Total = props => {
-	let sum = 0;
-	props.parts.map(x => (sum += x.exercises));
-	return (
-		<>
-			<p>Number of excersices {sum}</p>
-		</>
-	);
+const Statistics = ({ good, neutral, bad }) => {
+	const total = good + neutral + bad;
+	const average = (good - bad) / total;
+	const positive = (good / total) * 100 + ' %';
+
+	if (total === 0) {
+		return (
+			<>
+				<p>No feedback given</p>
+			</>
+		);
+	} else {
+		return (
+			<tbody>
+				<table>
+					<StatisticLine text="good" value={good} />
+					<StatisticLine text="nuetral" value={neutral} />
+					<StatisticLine text="bad" value={bad} />
+					<StatisticLine text="all" value={total} />
+					<StatisticLine text="average" value={average} />
+					<StatisticLine text="positive" value={positive} />
+				</table>
+			</tbody>
+		);
+	}
 };
 
 const App = () => {
-	const course = {
-		name: 'Half Stack application development',
-		parts: [
-			{
-				name: 'Fundamentals of React',
-				exercises: 10,
-			},
-			{
-				name: 'Using props to pass data',
-				exercises: 7,
-			},
-			{
-				name: 'State of a component',
-				exercises: 14,
-			},
-		],
+	const [good, setGood] = useState(0);
+	const [neutral, setNeutral] = useState(0);
+	const [bad, setBad] = useState(0);
+
+	const incrementGood = () => {
+		setGood(good + 1);
+	};
+	const incrementNeutral = () => {
+		setNeutral(neutral + 1);
+	};
+	const incrementBad = () => {
+		setBad(bad + 1);
 	};
 
 	return (
 		<div>
-			<Header course={course.name} />
-			<Content
-				part={course.parts[0].name}
-				excersices={course.parts[0].exercises}
-			/>
-			<Content
-				part={course.parts[1].name}
-				excersices={course.parts[1].exercises}
-			/>
-			<Content
-				part={course.parts[2].name}
-				excersices={course.parts[2].exercises}
-			/>
-			<Total parts={course.parts} />
+			<Title text="give feedback" />
+			<Button eventHandler={incrementGood} text="good" />
+			<Button eventHandler={incrementNeutral} text="neutral" />
+			<Button eventHandler={incrementBad} text="bad" />
+			<Title text="statistics" />
+			<Statistics good={good} neutral={neutral} bad={bad} />
 		</div>
 	);
 };
